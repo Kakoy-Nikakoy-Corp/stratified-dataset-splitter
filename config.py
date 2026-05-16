c2ce7d2c341d9582a5541743c60f38c13faca883
Maksim Shchibrik <partycle2006nt@gmail.com>
2026-05-16 19:11:58 +0500

    change URL

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ YOLO_CONF = {
 EXPORT_PATH = Path("./data/label-studio-export.json")
 DATASET_PATH = Path("./data/dataset.ndjson")
 OLD_URL = "http://192.168.0.104:1337"
-NEW_URL = "https://barstatic.oops.wtf"
+NEW_URL = "https://dataset.irbis.wild1.net"
 
 SPLIT_RATIO = [0.7, 0.15, 0.15]
 GAIN_WEIGHTS = [1, 3]
